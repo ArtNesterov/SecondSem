38d99bd41cee39eb7ec084fb4c87cbe92ef57e16
Артем Нестеров <artem.nestor15@mail.ru>
2026-05-06 16:18:25 +0300

    Update Point.h

--- Point.h
+++ Point.h
@@ -6,6 +6,11 @@
  */
 class Point
 {
+private:
+    double x; // координата X
+    double y; // координата Y
+    double z; // координата Z
+
 public:
     /**
      * @brief Конструктор точки
@@ -13,7 +18,38 @@ public:
      * @param y координата Y
      * @param z координата Z
      */
-    Point(const double x = 0.0, const double y = 0.0, const double z = 0.0);
+    Point(const double x = 0, const double y = 0, const double z = 0);
+
+    /**
+     * @brief Конструктор копирования
+     * @param other объект для копирования
+     */
+    Point(const Point& other);
+
+    /**
+     * @brief Конструктор перемещения
+     * @param other объект для перемещения
+     */
+    Point(Point&& other);
+
+    /**
+     * @brief Деструктор
+     */
+    ~Point() = default;
+
+    /**
+     * @brief Оператор присваивания копированием
+     * @param other объект для копирования
+     * @return текущий объект
+     */
+    Point& operator=(const Point& other);
+
+    /**
+     * @brief Оператор присваивания перемещением
+     * @param other объект для перемещения
+     * @return текущий объект
+     */
+    Point& operator=(Point&& other);
 
     /**
      * @brief Получить координату X
@@ -34,49 +70,49 @@ public:
     double GetZ() const;
 
     /**
-     * @brief Оператор равенства
+     * @brief Оператор сравнения на равенство
      * @param other другая точка
-     * @return true, если точки равны
+     * @return true если точки равны
      */
     bool operator==(const Point& other) const;
 
     /**
-     * @brief Оператор неравенства
+     * @brief Оператор сравнения на неравенство
      * @param other другая точка
-     * @return true, если точки не равны
+     * @return true если точки не равны
      */
     bool operator!=(const Point& other) const;
 
     /**
-     * @brief Оператор меньше
+     * @brief Оператор сравнения меньше
      * @param other другая точка
-     * @return true, если текущая точка меньше other
+     * @return true если текущая точка меньше other
      */
     bool operator<(const Point& other) const;
 
     /**
-     * @brief Оператор меньше или равно
+     * @brief Оператор сравнения меньше или равно
      * @param other другая точка
-     * @return true, если текущая точка меньше или равна other
+     * @return true если текущая точка меньше или равна other
      */
     bool operator<=(const Point& other) const;
 
     /**
-     * @brief Оператор больше
+     * @brief Оператор сравнения больше
      * @param other другая точка
-     * @return true, если текущая точка больше other
+     * @return true если текущая точка больше other
      */
     bool operator>(const Point& other) const;
 
     /**
-     * @brief Оператор больше или равно
+     * @brief Оператор сравнения больше или равно
      * @param other другая точка
-     * @return true, если текущая точка больше или равна other
+     * @return true если текущая точка больше или равна other
      */
     bool operator>=(const Point& other) const;
 
     /**
-     * @brief Перегрузка оператора вывода в поток
+     * @brief Оператор вывода точки в поток
      * @param os поток вывода
      * @param point точка
      * @return поток вывода
@@ -84,15 +120,10 @@ public:
     friend std::ostream& operator<<(std::ostream& os, const Point& point);
 
     /**
-     * @brief Перегрузка оператора ввода из потока
+     * @brief Оператор ввода точки из потока
      * @param is поток ввода
      * @param point точка
      * @return поток ввода
      */
     friend std::istream& operator>>(std::istream& is, Point& point);
-
-private:
-    double x; 
-    double y; 
-    double z; 
 };
