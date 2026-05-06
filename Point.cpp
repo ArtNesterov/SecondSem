64a4589f275a5ae59a9d8b1a9ff4188e9fa7bfc2
Артем Нестеров <artem.nestor15@mail.ru>
2026-05-06 16:18:05 +0300

    Update Point.cpp

--- Point.cpp
+++ Point.cpp
@@ -1,9 +1,59 @@
 #include "Point.h"
-#include <cmath>
-#include <limits>
+
+using namespace std;
 
 Point::Point(const double x, const double y, const double z) : x(x), y(y), z(z)
 {
+}
+
+Point::Point(const Point& other)
+{
+    x = other.x;
+    y = other.y;
+    z = other.z;
+}
+
+Point::Point(Point&& other)
+{
+    x = other.x;
+    y = other.y;
+    z = other.z;
+
+    other.x = 0;
+    other.y = 0;
+    other.z = 0;
+}
+
+Point& Point::operator=(const Point& other)
+{
+    if (this == &other)
+    {
+        return *this;
+    }
+
+    x = other.x;
+    y = other.y;
+    z = other.z;
+
+    return *this;
+}
+
+Point& Point::operator=(Point&& other)
+{
+    if (this == &other)
+    {
+        return *this;
+    }
+
+    x = other.x;
+    y = other.y;
+    z = other.z;
+
+    other.x = 0;
+    other.y = 0;
+    other.z = 0;
+
+    return *this;
 }
 
 double Point::GetX() const
@@ -23,9 +73,7 @@ double Point::GetZ() const
 
 bool Point::operator==(const Point& other) const
 {
-    return (std::abs(x - other.x) < std::numeric_limits<double>::epsilon() &&
-        std::abs(y - other.y) < std::numeric_limits<double>::epsilon() &&
-        std::abs(z - other.z) < std::numeric_limits<double>::epsilon());
+    return x == other.x && y == other.y && z == other.z;
 }
 
 bool Point::operator!=(const Point& other) const
@@ -36,19 +84,15 @@ bool Point::operator!=(const Point& other) const
 bool Point::operator<(const Point& other) const
 {
     if (x != other.x)
-    {
         return x < other.x;
-    }
     if (y != other.y)
-    {
         return y < other.y;
-    }
     return z < other.z;
 }
 
 bool Point::operator<=(const Point& other) const
 {
-    return (*this < other || *this == other);
+    return (*this < other) || (*this == other);
 }
 
 bool Point::operator>(const Point& other) const
@@ -61,13 +105,13 @@ bool Point::operator>=(const Point& other) const
     return !(*this < other);
 }
 
-std::ostream& operator<<(std::ostream& os, const Point& point)
+ostream& operator<<(ostream& os, const Point& point)
 {
     os << point.x << " " << point.y << " " << point.z;
     return os;
 }
 
-std::istream& operator>>(std::istream& is, Point& point)
+istream& operator>>(istream& is, Point& point)
 {
     is >> point.x >> point.y >> point.z;
     return is;
